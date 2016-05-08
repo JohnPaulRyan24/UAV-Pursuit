b71dfa3064a9f43fb8ead67c52481dfd3e6ae5d8
Andrew Klingelhofer <andrewcklingelhofer@gmail.com>
2016-05-08 16:45:33 -0700

    Added kalman filter, now need argparse so it can write csv data for kalman filter

--- CMT_with_stream.py
+++ CMT_with_stream.py
@@ -3,31 +3,58 @@
 
 import cv2
 
-
+import os.path
 import time
 import io
 import subprocess
 import CMT
-import numpy as np
+import numpy
 import util
+import numpy.matlib
+from numpy.linalg import inv
 
+# Kalman Filter Setup
+
+def kalman_predict(x,P,F,Q):
+    x = F*x;        #predicted state
+    P = F*P*F.transpose() + Q; #predicted estimate covariance
+    res = [x, P];
+    return res;
+
+def kalman_update(x,P,z,H,R):
+    y = z - H*x;    #measurement error/innovation
+    S = H*P*H.transpose() + R; #measurement error/innovation covariance
+    K = P*H.transpose()*inv(S); #optimal Kalman gain
+    x = x + K*y; #updated state estimate
+    P = (numpy.matlib.identity(x.shape[0]) - K*H)*P; #updated estimate covariance
+
+    res = [x, P];
+    return res;
+
+# Set up CMT parsing/necessary for writing csv data
+
+# define the filter
+
+x = numpy.matlib.zeros((6,1));
+F = numpy.matrix('1 0 0 0 1 0; 0 1 0 0 0 1; 0 0 1 0 1 0; 0 0 0 1 0 1; 0 0 0 0 1 0; 0 0 0 0 0 1');
+Q = numpy.matrix('0.25 0 0 0 0.5 0; 0 0.25 0 0 0 0.5; 0 0 0.25 0 0.5 0; 0 0 0 0.25 0 0.5; 0.5 0 0.5 0 1 0; 0 0.5 0 0.5 0 1');
+Q[:] = [a*0.01 for a in Q];
+H = numpy.matrix('1 0 0 0 0 0; 0 1 0 0 0 0; 0 0 1 0 0 0; 0 0 0 1 0 0');
+R = numpy.matlib.identity(4);
+R[:] = [b*42.25 for b in R];
+P = numpy.matlib.identity(6);
+P[:] = [c*10000 for c in P];
+
+n = 1;
 
 CMT = CMT.CMT()
 f =io.open('video.h264','rb')
 if(True):
-    #Use some initial image for right now
-    #im0 = cv2.imread('init.png')
-    #im_gray0 = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
-    #im_draw = np.copy(im0)
 
-    #Just some initial box coordinates
-    #tl = (450, 240)
-    #br = (730, 400)
-    #print 'using', tl, br, 'as init bb'
-    counter=0
+    counter = 0
 
     #Here we read frames from f and write them to a temp file
-    red= f.read()
+    red = f.read()
     w = io.open(str(counter)+'temp.h264','wb')
     w.write(red) 
     w.close()#-loglevel panic
@@ -42,7 +69,7 @@ if(True):
     print "here"
     status, im0 = cap.read()
     im_gray0 = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
-    im_draw = np.copy(im0)
+    im_draw = numpy.copy(im0)
 
     (tl, br) = util.get_rect(im_draw)
 
@@ -72,7 +99,7 @@ if(True):
             status, im = cap.read() 
             
         im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        im_draw = np.copy(im)
+        im_draw = numpy.copy(im)
 
         tic = time.time()
         CMT.process_frame(im_gray)
@@ -80,20 +107,69 @@ if(True):
 
         # Display results
 
+        # Draw Kalman Filter here instead
+        number = '%08d'%(n);
+        filename = ('output/bbox_' + number + '.csv');
+        if not os.path.isfile(filename):
+            break;
+
+        # read in the detected object's bounding box dimensions
+        fid = open(filename);
+        fid.readline(); #ignore the header
+        detections = [];
+        for i in fid:
+            detections.append(i.strip().split(' '));
+        fid.close();
+
+        meas_x1 = float(detections[0][0]);
+        meas_x2 = float(detections[2][0]);
+        meas_y1 = float(detections[0][1]);
+        meas_y2 = float(detections[2][1]);
+        z = numpy.matrix([[meas_x1], [meas_x2], [meas_y1], [meas_y2]]);
+
+        # step 1: predict
+        result = kalman_predict(x,P,F,Q);
+        x = result[0];
+        P = result[1];
+
+        # step 2: update (if measurement exists)
+        if all(j>0 for j in z):
+            result = kalman_update(x,P,z,H,R); 
+            x = result[0];
+            P = result[1];
+
+        est_z = H*x;
+        est_x1 = est_z[0];
+        est_x2 = est_z[1];
+        est_y1 = est_z[2];
+        est_y2 = est_z[3];
+
+        # draw a bounding box around the detected object
+        imgname = 'output/output_' + number + '.png';
+        img = cv2.imread(imgname, 1);
+
+        if all(k > 0 for k in est_z) and (est_x2>est_x1) and (est_y2>est_y1):
+            cv2.rectangle(img, (est_x1, est_y1), (est_x2, est_y2), (0,255,0), 2);
+
+        cv2.imshow('KalmanFilter',img);
+        cv2.waitKey(1);
+
+        n += 1;
+
         # Draw updated estimate
-        if CMT.has_result:
+        #if CMT.has_result:
 
-            cv2.line(im_draw, CMT.tl, CMT.tr, (255, 0, 0), 4)
-            cv2.line(im_draw, CMT.tr, CMT.br, (255, 0, 0), 4)
-            cv2.line(im_draw, CMT.br, CMT.bl, (255, 0, 0), 4)
-            cv2.line(im_draw, CMT.bl, CMT.tl, (255, 0, 0), 4)
+        #    cv2.line(im_draw, CMT.tl, CMT.tr, (255, 0, 0), 4)
+        #    cv2.line(im_draw, CMT.tr, CMT.br, (255, 0, 0), 4)
+        #    cv2.line(im_draw, CMT.br, CMT.bl, (255, 0, 0), 4)
+        #    cv2.line(im_draw, CMT.bl, CMT.tl, (255, 0, 0), 4)
 
-        util.draw_keypoints(CMT.tracked_keypoints, im_draw, (255, 255, 255))
+        #util.draw_keypoints(CMT.tracked_keypoints, im_draw, (255, 255, 255))
         # this is from simplescale
-        util.draw_keypoints(CMT.votes[:, :2], im_draw)  # blue
-        util.draw_keypoints(CMT.outliers[:, :2], im_draw, (0, 0, 255))
+        #util.draw_keypoints(CMT.votes[:, :2], im_draw)  # blue
+        #util.draw_keypoints(CMT.outliers[:, :2], im_draw, (0, 0, 255))
 
-        cv2.imshow('main', im_draw)
+        #cv2.imshow('main', im_draw)
 
         # Check key input
         k = cv2.waitKey(1)
