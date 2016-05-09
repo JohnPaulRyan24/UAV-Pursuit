9010539a5acc7419b09f1ddb681433b208e2047b
JohnPaulRyan24 <jpr349@nyu.edu>
2016-05-09 00:01:37 -0400

    Video file commands turns

--- video.js
+++ video.js
@@ -3,13 +3,36 @@ var bebop = require("../."),
     fs = require("fs");
 
 var output = fs.createWriteStream("./video.h264"),
-    drone = bebop.createClient(),
-    video = drone.getVideoStream();
+    drone = bebop.createClient();
+    
 
-video.pipe(output);
+
+
+
+function shimmy(){
+   var inp = fs.readFileSync('file.txt').toString();
+    if(inp==="0"){
+        drone.stop();
+    }else if(inp==="1"){
+        setTimeout(function(){
+            drone.clockwise(20);
+    }, 400);
+    setTimeout(function(){
+            drone.stop();
+    }, 800);
+fs.writeFile("file.txt", "0");
+    }else{
+        drone.land();
+    }
+}
 
 drone.connect(function() {
-  drone.MediaStreaming.videoEnable(1);
-
+    
+    drone.takeOff();
+    setInterval(shimmy, 1000);
+    setTimeout(function(){
+            var video = drone.getVideoStream();video.pipe(output);
+                drone.MediaStreaming.videoEnable(1);
+            console.log('Start video');},3000);
 
 });
